a690e30d16586c582de58427ddbc0c3f9b30218d
Valeria Guerrero <valeriaso.guerrero@umariana.edu.co>
2025-09-05 14:07:36 -0500

    build: publish site for GitHub Pages

--- IHM/vite.config.js
+++ IHM/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/tobyIHM//",
-});
+  plugins: [react()],
+  base: '/tobyIHM/',
+  build: { outDir: 'docs' }
+})
